00548d9cdf38b1922f3084cf9f20406a0d1eb577
MaxNate94 <incognito74108520@gmail.com>
2024-06-21 13:00:52 +0330

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,12 +33,12 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'APS + Gazebo',
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://github.com/MaxNate94/maxnate94.github.io/blob/main/public/tunnel-drone-1.gif',
+          link: 'https://youtube.com',
         },
         {
           title: 'Project Name',
